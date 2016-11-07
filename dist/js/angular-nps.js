88a80cbd35bad34946abc97e68391f3650c598be
Chris Sims <chris@sigaostudios.com>
2016-11-07 12:44:43 -0600

    Implemented service call and testing around the call

--- dist/js/angular-nps.js
+++ dist/js/angular-nps.js
@@ -3,7 +3,7 @@
 
     angular.module('angular-nps', []);        
 })();
-(function() {
+(function () {
     'use strict';
 
     angular.module('angular-nps')
@@ -12,19 +12,31 @@
     function $NpsApi($http, $q) {
         var api = {
             saveScore: saveScore,
-            test: test
+            test: test,
+            apiBase: '/sigaoapi/nps'
         };
 
         return api;
 
-        function test(){
-            return "Hello World!";
+        function test() {
+            return $http.get(api.apiBase)
+                .then(function (data) {
+                    console.log(data.data);
+                    return data.data;
+                }, function (error) {
+
+                    return error;
+                });
         }
 
-        function saveScore(score){
-            var deferred = $q.defer();
-            deferred.resolve({Value:"Success!"});
-            return deferred.promise;
+        function saveScore(score) {
+            return $http.post(api.apiBase, score)
+                .then(function (data) {
+                    return data.data;
+                }, function (error) {
+                    console.log(error.data);
+                    return error.data;
+                });
         }
     }
 
--- test/spec/nps-form/nps-apiSpec.js
+++ test/spec/nps-form/nps-apiSpec.js
@@ -1,57 +1,81 @@
 describe('$npsapi', function () {
-    var $service,
+    var $httpBackend,
+        $service,
         $rootScope,
+        scoreRequestHandler,
         result;
 
     beforeEach(module('angular-nps'));
 
-    /* jshint ignore:start */
-    beforeEach(inject(function (_$npsapi_, _$rootScope_) {
+    beforeEach(inject(function (_$npsapi_, _$rootScope_, _$httpBackend_) {
         $service = _$npsapi_;
         $rootScope = _$rootScope_;
-    }));
-    /* jshint ignore:end */
+        $httpBackend = _$httpBackend_;
 
-    it("should have a test function that returns hello world", function () {
-        expect($service.test()).toEqual("Hello World!");
+        testRequestHandler = $httpBackend.when('GET', '/sigaoapi/nps')
+            .respond('Hello World!');
+    }));
+
+    afterEach(function () {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it("should have a test function that returns hello world", function (done) {
+        console.log("running test");
+
+        $service.test()
+            .then(function (data) {
+                expect(data).toBe("Hello World!");
+            });
+        $httpBackend.flush();
+        done();
     });
 
     it("should return an error when submitting a score of 11.", function (done) {
+        scoreRequestHandler = $httpBackend.when('POST', '/sigaoapi/nps')
+            .respond(500, 'Score cannot be greater than 10.');
+
         $service.saveScore({
                 score: 11,
                 UserName: "Test"
             })
             .then(function (data) {
-                expect(data.Value).toBe("Score cannot be greater than 10.");
+                expect(data).toBe("Score cannot be greater than 10.");
             });
 
-        $rootScope.$digest();
+        $httpBackend.flush();
         done();
     });
 
 
     it("should return an error when submitting a score of -1", function (done) {
+        scoreRequestHandler = $httpBackend.when('POST', '/sigaoapi/nps')
+            .respond(500, 'Score cannot be less than 0.');
+
         $service.saveScore({
             score: -1,
             UserName: "Test"
         }).then(function (data) {
-            expect(data.Value).toEqual("Score cannot be less than 0.");
+            expect(data).toEqual("Score cannot be less than 0.");
         });
 
-        $rootScope.$digest();
+        $httpBackend.flush();
         done();
     });
 
 
     it("should return success when submitting a valid score.", function (done) {
+        scoreRequestHandler = $httpBackend.when('POST', '/sigaoapi/nps')
+            .respond(202, 'Success!');
         $service.saveScore({
             score: 5,
             UserName: "Test"
         }).then(function (data) {
-            expect(data.Value).toEqual("Success!");
+            expect(data).toEqual("Success!");
         });
 
-        $rootScope.$digest();
+        $httpBackend.flush();
         done();
     });
 
